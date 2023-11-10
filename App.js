92b589d07ae2c0c7edee0c3ce62e734bf853546e
phamcongtue <phantue2003@gmail.com>
2023-11-10 07:19:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Splash from './screens/Splash';
+import OnBording from './screens/OnBording';
+import SignIn from './screens/SignIn';
+import Number from './screens/Number';
+import Verification from './screens/Verification';
+
+export default function App() {
+  return <Splash/>
+}
